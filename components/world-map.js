2f3c53653d44ec2798f929a997fe49e76388f608
Rafael Bardini <rafael@rbardini.com>
2021-04-25 23:49:48 +0200

    Maintain world map aspect ratio

--- components/world-map.js
+++ components/world-map.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Box } from "@chakra-ui/react";
+import { AspectRatio, Box } from "@chakra-ui/react";
 import { useTheme } from "@amcharts/amcharts4/core";
 import animatedTheme from "@amcharts/amcharts4/themes/animated";
 import useChart from "../hooks/use-chart";
@@ -10,5 +10,9 @@ export default function WorldMap({ countriesData, onCountryClick }) {
   const containerRef = useRef(null);
   useChart(containerRef, countriesData, onCountryClick);
 
-  return <Box ref={containerRef} height="500px" />;
+  return (
+    <AspectRatio marginInline="auto" maxInlineSize="130vh" ratio={2 / 1}>
+      <Box ref={containerRef} />
+    </AspectRatio>
+  );
 }
