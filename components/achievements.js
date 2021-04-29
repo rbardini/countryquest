b7c563facd925b8b19261f45cd49f6eee5f9931a
Rafael Bardini <rafael@rbardini.com>
2021-04-29 14:12:47 +0200

    Sort achievements by progress

--- components/achievements.js
+++ components/achievements.js
@@ -14,28 +14,46 @@ export default function Achievements({ visitedCountriesData }) {
     <VStack align="stretch">
       <Heading>Achievements</Heading>
       <SimpleGrid minChildWidth="20em" spacing={2}>
-        {achievements.map(
-          ({ description, formatValue, max, name, unit, value }) => (
-            <Box key={name} borderRadius="lg" borderWidth="1px" padding={4}>
-              <Heading as="h3" fontSize="1xl">
-                🏆 {name}
-              </Heading>
-              <Text>{description}</Text>
-              <HStack>
-                <Progress
-                  borderRadius="full"
-                  flex={1}
-                  max={1}
-                  value={value(visitedCountriesData) / max}
-                ></Progress>
-                <Text fontSize="xs" textAlign="end">
-                  {formatValue(value(visitedCountriesData))} /{" "}
-                  {formatValue(max)} {unit}
-                </Text>
-              </HStack>
-            </Box>
-          )
-        )}
+        {achievements
+          .map((achievement) => {
+            const value = achievement.value(visitedCountriesData);
+            return {
+              ...achievement,
+              formattedMaxValue: achievement.formatValue(achievement.max),
+              formattedValue: achievement.formatValue(value),
+              progress: value / achievement.max,
+              value,
+            };
+          })
+          .sort((a, b) => b.progress - a.progress)
+          .map(
+            ({
+              description,
+              formattedMaxValue,
+              formattedValue,
+              name,
+              progress,
+              unit,
+            }) => (
+              <Box key={name} borderRadius="lg" borderWidth="1px" padding={4}>
+                <Heading as="h3" fontSize="1xl">
+                  🏆 {name}
+                </Heading>
+                <Text>{description}</Text>
+                <HStack>
+                  <Progress
+                    borderRadius="full"
+                    flex={1}
+                    max={1}
+                    value={progress}
+                  ></Progress>
+                  <Text fontSize="xs" textAlign="end">
+                    {formattedValue} / {formattedMaxValue} {unit}
+                  </Text>
+                </HStack>
+              </Box>
+            )
+          )}
       </SimpleGrid>
     </VStack>
   );
