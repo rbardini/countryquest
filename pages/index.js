996475d83a97b666b63555ba8ff7a3d15203e500
Rafael Bardini <rafael@rbardini.com>
2021-05-04 18:22:57 +0200

    Replace favicon with world map emoji

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,10 @@ export default function Home() {
     <Box>
       <Head>
         <title>Countryquest</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗺️</text></svg>"
+        ></link>
       </Head>
       <Header />
       <Divider />
