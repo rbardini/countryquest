a0b85b04f833f5b66abfd928b8a385ad2fbdbedd
Rafael Bardini <rafael@rbardini.com>
2021-05-08 13:07:25 +0200

    Add chart zoom controls

--- hooks/use-chart.js
+++ hooks/use-chart.js
@@ -4,19 +4,34 @@ import { MouseCursorStyle, color, create } from "@amcharts/amcharts4/core";
 import {
   MapChart,
   MapPolygonSeries,
+  ZoomControl,
   projections,
 } from "@amcharts/amcharts4/maps";
 import geodata from "../data/geodata";
 
 export default function useChart(containerRef, countriesData, onCountryClick) {
-  const [gray100, blue500] = useToken("colors", ["gray.100", "blue.500"]);
+  const [white, gray100, gray200, blue500] = useToken("colors", [
+    "white",
+    "gray.100",
+    "gray.200",
+    "blue.500",
+  ]);
   const chartRef = useRef(null);
 
   useLayoutEffect(() => {
+    const zoomControl = new ZoomControl();
+    [zoomControl.plusButton, zoomControl.minusButton].forEach((button) => {
+      button.background.fill = color(white);
+      button.background.stroke = color(gray100);
+      button.background.states.getKey("hover").properties.fill = color(gray200);
+      button.stroke = color(blue500);
+    });
+
     const chart = create(containerRef.current, MapChart);
     chart.hiddenState.properties.opacity = 0;
     chart.geodata = geodata;
     chart.projection = new projections.NaturalEarth1();
+    chart.zoomControl = zoomControl;
 
     const series = chart.series.push(new MapPolygonSeries());
     series.useGeodata = true;
