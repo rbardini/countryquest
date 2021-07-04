fd8ffac8b3fa6237d990442e1227d0a72c14d86c
Rafael Bardini <rafael@rbardini.com>
2021-07-04 14:03:58 +0200

    Remove animated chart theme to improve performance

--- components/world-map.js
+++ components/world-map.js
@@ -1,11 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import { AspectRatio, Box } from "@chakra-ui/react";
-import { useTheme } from "@amcharts/amcharts4/core";
-import animatedTheme from "@amcharts/amcharts4/themes/animated";
 import useChart from "../hooks/use-chart";
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-useTheme(animatedTheme);
 
 export default forwardRef(function WorldMap(
   { onCountryClick, visitedCountriesData },
