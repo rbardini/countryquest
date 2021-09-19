c9434e6bc218aa2b7f50fe82a03ca86025fe5fc4
Rafael Bardini <rafael@rbardini.com>
2021-09-19 15:19:28 +0200

    Add meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,10 @@ export default function Home() {
     <Box>
       <Head>
         <title>Countryquest</title>
+        <meta
+          name="description"
+          content="Countryquest is an interactive scratch off map. Keep track of which places you have been, how much of the world you have conquered, and where to go next."
+        />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗺️</text></svg>"
