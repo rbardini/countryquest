7a44c324eb26e11856412467c4cd76f4a4c2f00e
Rafael Bardini <rafael@rbardini.com>
2021-09-29 23:47:53 +0200

    Add bundle analyzer and remove unused modules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['atoms', 'components', 'data', 'hooks', 'lib', 'pages'],
   },
-}
+  webpack: (config, options) => ({
+    ...config,
+    externals: [
+      ...config.externals,
+      ({ request }, callback) => {
+        // Remove unused chart exporting modules
+        // https://www.amcharts.com/docs/v4/getting-started/integrations/using-webpack/#Large_file_sizes
+        if (/xlsx|canvg|pdfmake/.test(request)) {
+          return callback(null, 'commonjs ' + request)
+        }
+
+        callback()
+      },
+    ],
+  }),
+})
