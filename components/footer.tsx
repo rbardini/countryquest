c6e9704f5df97aa2e6f86ae3c0d9421c4ffa04d9
Rafael Bardini <rafael@rbardini.com>
2022-04-03 21:40:51 +0200

    Add REST Countries link to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,17 @@ export default function Footer() {
   const linkColor = useColorModeValue('blue.500', 'blue.200')
 
   return (
-    <Flex gridGap={4} justifyContent="center" padding={8}>
+    <Flex fontSize="sm" gap={4} justifyContent="center" padding={8} wrap="wrap">
       <Text as="span">
         Made with ❤️ by{' '}
         <Link color={linkColor} href="https://rbardini.com/">
           Rafael Bardini
+        </Link>
+      </Text>
+      <Text as="span">
+        Data by{' '}
+        <Link color={linkColor} href="https://restcountries.com/">
+          REST Countries
         </Link>
       </Text>
       <Text as="span">
