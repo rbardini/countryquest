8f9e9c30f48c0cce6427b07306115eca17404a61
Rafael Bardini <rafael@rbardini.com>
2022-04-12 11:39:21 +0200

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -7,19 +7,4 @@ module.exports = withBundleAnalyzer({
     dirs: ['atoms', 'components', 'data', 'hooks', 'lib', 'pages'],
   },
   generateBuildId: () => 'build',
-  webpack: (config, options) => ({
-    ...config,
-    externals: [
-      ...config.externals,
-      ({ request }, callback) => {
-        // Remove unused chart exporting modules
-        // https://www.amcharts.com/docs/v4/getting-started/integrations/using-webpack/#Large_file_sizes
-        if (/xlsx|canvg|pdfmake/.test(request)) {
-          return callback(null, 'commonjs ' + request)
-        }
-
-        callback()
-      },
-    ],
-  }),
 })
