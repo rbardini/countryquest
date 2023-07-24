65e05a2b69530aaf9685fd413eb810e1c62a5e5a
Rafael Bardini <rafael@rbardini.com>
2023-07-24 23:38:00 +0200

    Use `localStorage` for auth storage

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -4,6 +4,6 @@ export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
-    auth: { persistSession: false },
+    auth: { storage: globalThis.localStorage },
   },
 )
